718ca38d82199edeafd8ad235c3ecd074d730322
ryanwang058 <ryanwang5@cmail.carleton.ca>
2022-10-11 09:40:48 -0400

    Mod: crawler.py - len(dicPages)

--- crawler.py
+++ crawler.py
@@ -15,20 +15,20 @@ def crawl(seed):
     
     #while there's still something in the queue
     while(len(lstQueue)>0):
-        #go through the links we need to go through
-        for strSubPage in lstQueue:
-            #If the links found in page aren't in the queue or in the pages visited, add it to the list
-            dicPages[strSubPage]=getLinks(strSubPage)
-            #since we've gone on that page, let's add it to the pages we've visited
-            lstPagesVisited.append(strSubPage)
-            lstQueue.pop(0)
-            for strLink in dicPages[strSubPage]:
-                #if it's not in the queue already or not in the pages visited, then
-                if((strLink not in lstQueue) and (strLink not in lstPagesVisited) and (strLink not in dicPages)):
-                    lstQueue.append(strLink)
+        #each time we go through one link
+        strSubPage = lstQueue[0]
+        #If the links found in page aren't in the queue or in the pages visited, add it to the list
+        dicPages[strSubPage]=getLinks(strSubPage)
+        #since we've gone on that page, let's add it to the pages we've visited
+        lstPagesVisited.append(strSubPage)
+        lstQueue.pop(0)
+        for strLink in dicPages[strSubPage]:
+            #if it's not in the queue already or not in the pages visited, then
+            if((strLink not in lstQueue) and (strLink not in lstPagesVisited) and (strLink not in dicPages)):
+                lstQueue.append(strLink)
     
-    for key in dicPages:
-        print(key)
+    # for key in dicPages:
+    #     print(key)
     #the number of pages we visited will be the number of pages there are since we visited all of them
     return len(dicPages)
 
