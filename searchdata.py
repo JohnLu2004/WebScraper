6362cf5642ea2eff2944728da8470ec9ff0760f2
MehBirb <john.lu10212004@gmail.com>
2022-10-11 11:56:39 -0400

    get_incoming_links
    I've created a new function that gets the links from subpages

--- searchdata.py
+++ searchdata.py
@@ -1,15 +1,15 @@
 #this function returns a list of links
-def get_outgoing_links(seed):
+def get_outgoing_links(URL):
     
     #We'll open up the file for reading
-    lstLines=webdev.read_url(seed).strip().split("\n")
+    lstLines=webdev.read_url(URL).strip().split("\n")
     
     #I'll also have 2 variables to keep track of the beginning and end of a link
     intStart = 0
     intEnd = 0
     
     
-    strBeginning = seed[0:seed.rfind("/")+1]
+    strBeginning = URL[0:URL.rfind("/")+1]
     strLink = ""
     lstLinks=[]
     
@@ -32,3 +32,21 @@ def get_outgoing_links(seed):
             #now we add the link to our list of links
             lstLinks.append(strLink)
     return lstLinks
+    
+def get_incoming_links(URL):
+    lstQueue = get_outgoing_links(URL)
+    lstPages = []
+    lstAllPages = []
+    
+    #for every link in the main page, we will...
+    for strLinks in lstQueue:
+        #get a list of all the links in the subpage of a page
+        lstPages = get_outgoing_links(strLinks)
+        #for every link in that subpage, we will...
+        for strPage in lstPages:
+            #if that link isn't in the pages part yet, then...
+            if strPage not in lstAllPages:
+                #add it to the list of pages
+                lstAllPages.append(strPage)
+    #return a list of all the pages in subpages
+    return lstAllPages
