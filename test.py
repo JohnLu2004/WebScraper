e9d4092d33dd413447da2bdf0e26cc0151ee75de
ryanwang058 <ryanwang5@cmail.carleton.ca>
2022-10-19 17:06:48 -0400

    push all the code

--- test.py
+++ test.py
@@ -4,11 +4,12 @@ import searchdata
 import webdev
 import time
 start = time.time()
-print(crawler.crawl("http://people.scs.carleton.ca/~davidmckenney/fruits/N-0.html"))
+print(crawler.crawl("http://people.scs.carleton.ca/~davidmckenney/fruits3/N-0.html"))
 end = time.time()
 print("crawl:",end-start)
 
-start = time.time()
-print(search.search("apple",True))
-end = time.time()
-print("search:",end-start)
+# start = time.time()
+# print(search.search("apple",True))
+# end = time.time()
+# print("search:",end-start)
+# print(search.search("coconut coconut orange blueberry lime lime lime tomato",True))
